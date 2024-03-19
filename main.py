4a73fb440ea64a5162a9b447ef4ac6143cd15532
Dominik Wojnowski <d.wojnowski.priv@gmail.com>
2024-03-19 21:12:52 +0100

    added calculate function

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-{
-  'mean': [axis1, axis2, flattened],
-  'variance': [axis1, axis2, flattened],
-  'standard deviation': [axis1, axis2, flattened],
-  'max': [axis1, axis2, flattened],
-  'min': [axis1, axis2, flattened],
-  'sum': [axis1, axis2, flattened]
-}
+import pandas as pd
+
+def calculate(data):
+    print(pd.DataFrame(data).info())
+    print(pd.DataFrame(data).describe(  ))
+
 
 calculate([0,1,2,3,4,5,6,7,8])
 
-{
-  'mean': [[3.0, 4.0, 5.0], [1.0, 4.0, 7.0], 4.0],
-  'variance': [[6.0, 6.0, 6.0], [0.6666666666666666, 0.6666666666666666, 0.6666666666666666], 6.666666666666667],
-  'standard deviation': [[2.449489742783178, 2.449489742783178, 2.449489742783178], [0.816496580927726, 0.816496580927726, 0.816496580927726], 2.581988897471611],
-  'max': [[6, 7, 8], [2, 5, 8], 8],
-  'min': [[0, 1, 2], [0, 3, 6], 0],
-  'sum': [[9, 12, 15], [3, 12, 21], 36]
-}
+# {
+#   'mean': [axis1, axis2, flattened],
+#   'variance': [axis1, axis2, flattened],
+#   'standard deviation': [axis1, axis2, flattened],
+#   'max': [axis1, axis2, flattened],
+#   'min': [axis1, axis2, flattened],
+#   'sum': [axis1, axis2, flattened]
+# }
+#
+# {
+#   'mean': [[3.0, 4.0, 5.0], [1.0, 4.0, 7.0], 4.0],
+#   'variance': [[6.0, 6.0, 6.0], [0.6666666666666666, 0.6666666666666666, 0.6666666666666666], 6.666666666666667],
+#   'standard deviation': [[2.449489742783178, 2.449489742783178, 2.449489742783178], [0.816496580927726, 0.816496580927726, 0.816496580927726], 2.581988897471611],
+#   'max': [[6, 7, 8], [2, 5, 8], 8],
+#   'min': [[0, 1, 2], [0, 3, 6], 0],
+#   'sum': [[9, 12, 15], [3, 12, 21], 36]
+# }
